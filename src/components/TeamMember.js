4e700dd89709fb0b66e11c06522a02aa8573299f
Joe L <joseph.lasata@gmail.com>
2020-10-13 14:19:55 -0500

    added necessary components and began psuedo code

---
+++ src/components/TeamMember.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+function TeamMember() {
+  return (
+    <div className="TeamMember">
+      <h3>Team Member:</h3>
+      {/* Add details here when available */}
+    </div>
+  );
+}
+
+export default TeamMember;
